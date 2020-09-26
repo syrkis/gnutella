7ba3816f08868f20cbd850ca117850bde74fb6f1
Noah Syrkis <noah@syrkis.com>
2020-09-26 16:12:15 +0200

    add plotter function to explorere

--- bin/explorer.py
+++ bin/explorer.py
@@ -12,16 +12,30 @@ import os
 # construct
 path = '../data/'
 targets = [path + target for target in os.listdir(path)]
-S = {"G" + str(idx) : nx.read_edgelist(target, delimiter="\t", create_using=nx.Graph(name='test'))for idx, target in enumerate(targets)}
+S = {"G" + str(idx) : nx.read_edgelist(target,
+    delimiter="\t", create_using=nx.DiGraph(name='test'))
+    for idx, target in enumerate(targets)}
+for idx, G in enumerate(S):
+    S[G].name = "2002-08-" + targets[idx][-6:-4]
 
 # plotting
+def plotter(*args):
+    n = len(args)
+    for idx, arg in enumerate(args):
+        plt.figure(figsize=(14, 8))
 
+        plt.plot(list(arg.keys()), list(arg.values()), 'ob')
+        plt.title(f"Linear plot"); plt.xlabel("x"); plt.ylabel("y")
+        plt.savefig(f'../docs/plots/lin')
+
+        plt.title(f"Log plot"); plt.xlabel("x"); plt.ylabel("y") 
+        plt.loglog(list(arg.keys()), list(arg.values()), 'or')
+        plt.savefig(f'../docs/plots/log')
 
 # call stack
 def main():
-    for G in S:
-        print(nx.info(S[G]))
-    return 0
+    tmp = Counter(dict(S['G0'].degree).values())
+    plotter(tmp)
 
 if __name__ == "__main__":
     main()
