624b53391fc6ee737855b0fc5f7137c7a18aa5dc
Noah Syrkis <noah@syrkis.com>
2020-10-13 14:42:19 +0100

    compute the big five DiGraph centrality metrics

--- bin/centrality.py
+++ bin/centrality.py
@@ -10,7 +10,7 @@ This scripts focuses on various centrality measures of the Gnutella network. For
 import sys
 import random
 import networkx as nx
-import numps as np
+import numpy as np
 import pandas as pd
 from builder import S
 from collections import Counter
@@ -22,12 +22,18 @@ def centrality(G):
     betw = nx.betweenness_centrality(G, k = 1000)
     ideg = nx.in_degree_centrality(G)
     odeg = nx.out_degree_centrality(G)
-
+    clos = {nx.closeness_centrality(component(G), u = n) for n in sampler(component(G))} 
+    eige = nx.eigenvector_centrality(G, max_iter=200)
+    print(eige)
 
 # helpers
-def largecomp(G):
-    sotred_comps = sorted(nx.strongly_connected_components(G), key = len, reverse = True)
-    return G.subgraph(sorted_comps[0])
+def component(G):
+    S = sorted(nx.strongly_connected_components(G), key = len, reverse = True)
+    return G.subgraph(S[0])
+
+def sampler(G):
+    N = random.sample(G.nodes(), 1000)
+    return N
 
 # call stack
 def main():
